b488c2d946bb98ec96f082d75f6b65526b6de438
hamzabhai797 <hamzanoor11111122@gmail.com>
2025-03-02 16:30:54 +0500

    Changeing in calculator project

--- calculator project/script.js
+++ calculator project/script.js
@@ -2,3 +2,10 @@ let display = document.getElementById('inputBox');
 let buttons = document.querySelectorAll('button');
 
 let buttonsArray = Array.from(buttons);
+
+buttonsArray.forEach(btn => {
+
+    btn.addEventListener('click',(e) => {
+        console.log(e.target)
+    });
+});
