3c76b04d2cbcacec7a08ff8faca8ab8784282895
hamzabhai797 <hamzanoor11111122@gmail.com>
2025-03-05 10:45:39 +0500

    complete calculator

--- calculator project/script.js
+++ calculator project/script.js
@@ -10,10 +10,22 @@ buttonsArray.forEach(btn => {
 
         if(e.target.innerHTML == 'DEL'){
             string = string.substring(0, string.length - 1);
+            display.value = string;
         }
-
-        string += e.target.innerHTML;
-        display.value = string;
-
+        else if (e.target.innerHTML == 'AC'){
+            string = '';
+            display.value = string;
+        }
+        else if(e.target.innerHTML == '='){
+            try{
+                display.value = eval(string);
+            }catch{
+                display.value = 'Error';
+            }
+        }        
+        else{
+            string += e.target.innerHTML;
+            display.value = string;
+        }
     });
 });
